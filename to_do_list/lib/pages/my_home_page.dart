9e8ff06b6dd3a689677869c4276792e181aa20b1
Ricardo Chavez <74266998+ExHultant@users.noreply.github.com>
2024-02-05 21:00:49 -0400

    Add path_provider_foundation plugin and dependencies

--- to_do_list/lib/pages/my_home_page.dart
+++ to_do_list/lib/pages/my_home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:to_do_list/data/database.dart';
+import 'package:to_do_list/utils/dialog_box.dart';
 import 'package:to_do_list/utils/todo_title.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,36 +12,119 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //list of tasks
-  List TodoList = [
-    {'taskName': 'Task 1', 'taskCompleted': false},
-    {'taskName': 'Task 2', 'taskCompleted': false},
-    {'taskName': 'Task 3', 'taskCompleted': false},
-    {'taskName': 'Task 4', 'taskCompleted': false},
-    {'taskName': 'Task 5', 'taskCompleted': false},
-  ];
+  //reference the hive box
+  final _myBox = Hive.box('myBox');
+  ToDoDataBase db = ToDoDataBase();
+
+  @override
+  void initState() {
+    if (_myBox.get('todo') == null) {
+      db.createInitialData();
+    } else {
+      //there already exists data in the database
+      db.loadData();
+    }
+    //IS THIS THE FIRST TIME EVER OPENING THE APP, THEN CREATE INITIAL DATA
+    super.initState();
+  }
+
+  //text controller
+  final _controller = TextEditingController();
+
 //function to change the state of the checkbox
   void checkBoxChanged(bool? value, int index) {
     setState(() {
-      TodoList[index]['taskCompleted'] = !TodoList[index]['taskCompleted'];
+      db.toDoList[index]['taskCompleted'] =
+          !db.toDoList[index]['taskCompleted'];
     });
+    db.updateData();
+  }
+
+//save the new task
+  void saveNewTask() {
+    if (_controller.text.isEmpty) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('Por favor ingrese una tarea'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  db.updateData();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      setState(() {
+        db.toDoList.add({'taskName': _controller.text, 'taskCompleted': false});
+        _controller.clear();
+        Navigator.of(context).pop();
+      });
+    }
+  }
+
+//Create new task
+  void addNewTask() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return DialogBox(
+            controller: _controller,
+            onSave: saveNewTask,
+            onCancel: () {
+              _controller.clear();
+              Navigator.of(context).pop();
+            },
+          );
+        });
+  }
+
+  //delete a task
+  void deleteTask(int index) {
+    setState(() {
+      db.toDoList.removeAt(index);
+    });
+    db.updateData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[200],
+      backgroundColor: Colors.green[200],
       appBar: AppBar(
-        title: Text('TO DO'),
+        title: const Center(
+          child: Text(
+            'TO DO LIST APP',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        backgroundColor: Colors.green[600],
         elevation: 0,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewTask,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green[300],
+      ),
       body: ListView.builder(
-          itemCount: TodoList.length,
+          itemCount: db.toDoList.length,
           itemBuilder: (context, index) {
             return TodoTitle(
-              taskName: TodoList[index]['taskName'],
-              taskCompleted: TodoList[index]['taskCompleted'],
+              taskName: db.toDoList[index]['taskName'],
+              taskCompleted: db.toDoList[index]['taskCompleted'],
               onChanged: (value) => checkBoxChanged(value, index),
+              onDelete: ((p0) => deleteTask(index)),
+              index: index,
             );
           }),
     );
